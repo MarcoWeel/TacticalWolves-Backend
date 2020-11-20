c3d1daceb674752a97c7987dbad8e60507397b98
MarcoWeel <deathstarmarco@gmail.com>
2020-11-20 09:56:45 +0100

    basic project files added

---
+++ post-service/src/test/java/com/tacticalwolves/postservice/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tacticalwolves.postservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
